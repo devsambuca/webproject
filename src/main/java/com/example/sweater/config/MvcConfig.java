d263cb4dbdb64b7f0dba7f809605b0da2c09b2f0
Vladimir Fominykh <stoyakovi4@gmail.com>
2018-08-09 15:46:06 +0300

    Checked error

--- src/main/java/com/example/sweater/config/MvcConfig.java
+++ src/main/java/com/example/sweater/config/MvcConfig.java
@@ -1,14 +1,12 @@
 package com.example.sweater.config;
 
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * Created by z-FominykhVJ on 09.08.2018.
- */
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
     }
 }
