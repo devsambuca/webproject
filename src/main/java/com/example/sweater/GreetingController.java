ecc7d4385d12fca062fe0f66fe2f2542264def61
Fominykh <stoyakovi4@gmail.com>
2018-08-09 07:57:48 +0300

    Added method findByTag for finding a message by tag -Solved error no key, line 19"

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -17,22 +16,25 @@ public class GreetingController {
     private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-                           Map<String, Object> model
+    public String greeting(
+            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            Map<String, Object> model
     ) {
         model.put("name", name);
         return "greeting";
     }
+
     @GetMapping
-    public String main(Map<String, Object> model){
+    public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
 
         model.put("messages", messages);
 
         return "main";
     }
+
     @PostMapping
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
 
         messageRepo.save(message);
@@ -42,14 +44,19 @@ public class GreetingController {
         model.put("messages", messages);
 
         return "main";
-
     }
 
     @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model){
-        List<Message> messages = messageRepo.findByTag(filter);
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
 
-        model.put("message", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
 
         return "main";
     }
