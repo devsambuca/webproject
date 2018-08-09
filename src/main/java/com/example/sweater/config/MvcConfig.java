cc4b8038a1e766a900ca1709771a97a6e70e84f7
Vladimir Fominykh <stoyakovi4@gmail.com>
2018-08-09 14:19:51 +0300

    Added class MvcController

---
+++ src/main/java/com/example/sweater/config/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.example.sweater.config;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by z-FominykhVJ on 09.08.2018.
+ */
+public class MvcConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
