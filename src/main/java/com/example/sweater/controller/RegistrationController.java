6f2fe86a34b1efcb3fcd824b5b6b1487a0b9d0f4
Vladimir Fominykh <stoyakovi4@gmail.com>
2018-08-14 11:56:18 +0300

    Added class RegistrationController

---
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -0,0 +1,19 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegistrationController {
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user) {
+        return "redirect:/login";
+    }
+}
