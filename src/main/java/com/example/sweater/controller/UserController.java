24e5f4240c07d3b1a102baeda63e4c39b70c4b35
Vladimir Fominykh <stoyakovi4@gmail.com>
2018-08-15 12:51:12 +0300

    Added class UserController

---
+++ src/main/java/com/example/sweater/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.User;
+import com.example.sweater.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping
+    public String userList(Model model) {
+        model.addAttribute("users", userRepo.findAll());
+
+        return "userList";
+    }
+    @GetMapping("{user}")                                              //Для данного метода мы ожидаем меппинг по мимо части "/user"
+    public String userEditForm(@PathVariable User user, Model model) { // В PathVariable мы можем указать сразу User, тогда он будет
+                                                                       // брать из бд без дополнитольного репозитория
+        model.addAttribute("user", user);
+
+        return "userList";
+    }
+}
